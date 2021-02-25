7652b24c64a5477671b62214ac0539a16bde5c17
Mingzhang Deng <wu20880@bristol.ac.uk>
2021-02-26 07:40:59 +0800

    useful

--- output.py
+++ output.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def output(output_result:dict):
 
 
@@ -21,3 +22,23 @@ def output(output_result:dict):
 if __name__ == '__main__':
     content = {0:[["road1",12]],1:[["road 3",15],["road 5",20]]}
     output(content)
+=======
+inputDic = {'intersection': [['streetName', 'time'], []], 'intersection2': [['streetName', 'time']]}
+
+
+def writeFile(inputDic):
+    output = open('file.txt', 'w')
+    length = len(inputDic)
+    output.write('%d\n' % length)
+    count = 0
+    for i in inputDic:
+        temp = list(inputDic.keys())[i]
+        output.write(str(temp))
+        for j in inputDic[i]:
+            if isinstance(inputDic[i][j], list):
+                count += 1
+        output.write('%d\n' % count)
+
+
+writeFile(inputDic)
+>>>>>>> Add files via upload
